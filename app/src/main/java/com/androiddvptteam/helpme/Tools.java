66370799b4d87087c9c8c876dac97b47ac2a438b
CC <261909098@qq.com>
2017-05-14 23:04:24 +0800

    Mission类里加了一个MissionManager的内部静态类，提供对于任务的发布，接收，放弃，完成，取消操作

--- app/src/main/java/com/androiddvptteam/helpme/Tools.java
+++ app/src/main/java/com/androiddvptteam/helpme/Tools.java
@@ -1,10 +1,23 @@
 package com.androiddvptteam.helpme;
 
+import android.graphics.Bitmap;
+
 /**
  * 工具类，放各种静态工具函数
  */
-public abstract class Tools
+public class Tools
 {
+	/**
+	 * 从后端获取用户头像
+	 * @param id	用户ID
+	 * @return 		头像的位图
+	 * */
+	public static Bitmap getUserAvatarFromWeb(String id)
+	{
+		netDelay(1000);
+		return null;
+	}
+
 	/**
 	 * 加密函数
 	 * */
@@ -19,5 +32,22 @@ public abstract class Tools
 	public static String decrypt(String str)
 	{
 		return str;
+	}
+
+	/**
+	 * 模拟网络延迟的函数，最终所有延迟的地方都会被真实的网络操作代替
+	 * 所以涉及到这个函数（以后就是网络操作）的操作都必须异步执行
+	 * @param	ms	模拟延迟的毫秒数
+	 * */
+	public static void netDelay(int ms)
+	{
+		try
+		{
+			Thread.sleep(ms);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
